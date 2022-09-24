18a05eabb3db4c08f0a51812a39e3cee63695539
AbdulahadAbduqahhorov <abdulahadabduqahhorov2001@gmail.com>
2022-09-24 18:34:44 +0500

    last updates added

--- main.go
+++ main.go
@@ -8,35 +8,34 @@ import (
 
 func main() {
 
-	a, err := bigint.NewInt("0")
+	a, err := bigint.NewInt("12")
 	if err != nil {
 		panic(err)
 	}
 
-	b, err := bigint.NewInt("-123")
+	b, err := bigint.NewInt("0")
 	if err != nil {
 		panic(err)
 	}
 
-	// err = a.Set("-0000")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// err = b.Set("34324")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	
+	err = a.Set("67687879789")
+	if err != nil {
+		panic(err)
+	}
+	err = b.Set("34324")
+	if err != nil {
+		panic(err)
+	}
 
-	// c := bigint.Add(a, b)
-	// d := bigint.Sub(a, b)
-	// e := bigint.Multiply(a, b)
+	c := bigint.Add(a, b)
+	d := bigint.Sub(a, b)
+	e := bigint.Multiply(a, b)
 	d, f := bigint.ModAndDivision(a, b)
-	// fmt.Println(a)
-	// fmt.Println(b)
-	// fmt.Println(c)
-	// fmt.Println(d)
-	// fmt.Println(e)
-	// fmt.Printf("Quotient: %v Remainder: %v\n",d.Value,f.Value)
-	fmt.Println(d.Value,f.Value)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Printf("Quotient: %v Remainder: %v\n",d.Value,f.Value)
+	fmt.Println(d.Value, f.Value)
 }
